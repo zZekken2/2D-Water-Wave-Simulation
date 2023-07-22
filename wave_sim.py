ec0c840ee6b62f4d6a388b99d98db61bc08fa207
zZekken2 <94545042+zZekken2@users.noreply.github.com>
2023-07-22 12:46:45 +0100

    Add files via upload

---
+++ wave_sim.py
@@ -0,0 +1,163 @@
+import pygame
+from pygame.locals import *
+from threading import Thread
+import time
+
+#-----CONSTANTES-----
+WIDTH = 600
+HEIGHT = 600
+FPS = 60
+WATER_COLOR = (0, 106, 255)
+SCREEN_COLOR = (32, 32, 32)
+
+pygame.init()
+
+
+
+class Water:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Water Waves Simulation")
+
+        self.time = pygame.time.Clock()
+
+        self.target_height = HEIGHT // 2
+
+        self.initial_speed = 300 # NOTA: Passar a ser calculada em relação à massa e altura de lançamento do objeto
+
+        self.x_origin = 0
+        self.x_end = 600
+        self.start_index = 0
+
+        self.springs = []
+
+        self.spring_num = abs((self.x_origin - self.x_end) // 2) # Calculates the number of springs
+        self.spring_width = (self.x_origin + self.x_end) // self.spring_num # Calculates the width the springs have to be
+
+        self.spring_list()
+
+        self.running = True
+        self.run_simulation()
+
+    def run_simulation(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+                    pygame.quit()
+                    exit()
+
+                if event.type == MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos() # NOTA: em vez de utilizar a posição do rato utilizar um objeto em queda
+                    self.start_index = int(pos[0] / self.spring_width)
+                    self.start_waves()
+            
+            self.draw_level()
+
+            for spring in self.springs:
+                spring.draw_springs(self.screen)
+
+            self.time.tick(FPS)
+            pygame.display.flip()
+
+    def spring_list(self):
+        for x in range(self.x_origin, self.x_end, self.spring_width):
+            self.springs.append(Springs(x, self.target_height, self.spring_width))
+
+    def start_waves(self):
+        self.springs[self.start_index].speed = self.initial_speed
+        self.updates = Update_Control(self.springs)
+        self.updates.daemon = True
+        self.updates.start()
+    
+    def draw_level(self):
+        self.screen.fill(SCREEN_COLOR)
+
+
+
+class Springs:
+    def __init__(self, x, target_height, spring_width):
+        self.x = x
+        self.bottom_y = HEIGHT
+        self.wave_height = 0
+        self.height = target_height
+
+        self.speed = 0
+        self.spring_width = spring_width
+
+    def hooke_law(self, target_height, tension, dampening):
+        self.wave_height = self.height - target_height
+        self.speed += - tension * self.wave_height - self.speed * dampening # NOTA: diferenciar acelaração e velocidade e calcular a velocidade
+        self.height += self.speed # NOTA: aqui é aceleração
+    
+
+
+    def draw_springs(self, screen):
+        pygame.draw.line(screen, WATER_COLOR, (self.x, self.height), (self.x, self.bottom_y), self.spring_width)
+
+
+
+class Update_Control(Thread): # Update_Control inherits the functionalities of Thread
+    def __init__(self, springs):
+        Thread.__init__(self) # Iniciates the inherited properties
+        self.springs = springs # NOTA: springs é a lista de springs pois é a referência para a class Springs
+
+        self.target_height = HEIGHT // 2
+
+        self.springs_tension = 0.025 # Testing Value
+        self.springs_dampening = 0.020 # Testing value
+
+        self.spread_speed = 0.25 # Testing Value # NOTA: Calcular em relação à força (isto é aceleração)
+
+    def spring_update(self):
+        for i in range(len(self.springs)):
+            self.springs[i].hooke_law(self.target_height, self.springs_tension, self.springs_dampening)
+
+    def neighbor_list(self):
+        self.lDeltas = list(self.springs) # Creates the left neighbors of the activated spring
+        self.rDeltas = list(self.springs) # Creates the right neighbors
+
+    def neighbor_update(self):
+        for _ in range(5):
+        #-----UPDATES-VELOCITY-----
+            for i in range(len(self.springs)):
+                if i > 0:
+                    self.lDeltas[i] = self.spread_speed * (self.springs[i].height - self.springs[i-1].height)
+                    self.springs[i-1].speed += self.lDeltas[i]
+
+                if i <len(self.springs) - 1:
+                    self.rDeltas[i] = self.spread_speed * (self.springs[i].height - self.springs[i+1].height)
+                    self.springs[i+1].speed += self.rDeltas[i]
+
+        #-----UPDATES-HEIGHT-----
+            for i in range(len(self.springs)):
+                if i > 0:
+                    self.springs[i - 1].height += self.lDeltas[i]
+                if i < len(self.springs) - 1:
+                    self.springs[i + 1].height += self.rDeltas[i]
+    
+    def stop(self):
+        for i in range(len(self.springs)):
+            self.springs[i].speed = 0
+            self.springs[i].height = self.target_height
+            self.springs[i].wave_height = 0
+
+    def run(self): # One of the property methods inherited by Thread
+        while True: # Since it will be used as daemon it won't be necessary the loop management
+            self.spring_update()
+            self.neighbor_list()
+            self.neighbor_update()
+            count = 0
+
+            for i in range(len(self.springs)):
+                if not int(self.springs[i].speed) and not int(self.springs[i].wave_height):
+                    count += 1
+            if count == len(self.springs):
+                self.stop()
+        
+            time.sleep(0.01)
+
+
+
+if __name__ == "__main__":
+    Water()
